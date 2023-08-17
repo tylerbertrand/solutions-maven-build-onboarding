5f8e72b9f02bf7508895f8315af4bc1d6a0e335e
Tyler Bertrand <tbertrand@gradle.com>
2023-08-17 15:28:47 -0500

    Normalize cassandra-connection.properties
    Ensure cache hit by normalizing randomly generated port number
    
    Remove timestamp from Properties.store() when storing to cassandra-connection.properties

--- spring-data-cassandra/src/test/java/org/springframework/data/cassandra/support/CassandraConnectionProperties.java
+++ spring-data-cassandra/src/test/java/org/springframework/data/cassandra/support/CassandraConnectionProperties.java
@@ -15,12 +15,18 @@
  */
 package org.springframework.data.cassandra.support;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.HexFormat;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.springframework.core.convert.converter.Converter;
@@ -225,5 +231,83 @@ public class CassandraConnectionProperties extends Properties {
 
 	public enum CassandraType {
 		EMBEDDED, EXTERNAL, TESTCONTAINERS
+	}
+
+	/*
+	 * Overridden to remove timestamp from properties file
+	 */
+	@Override
+	public void store(OutputStream out, String comments)
+			throws IOException
+	{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+		synchronized (this) {
+			for (Map.Entry<Object, Object> e : entrySet()) {
+				String key = (String)e.getKey();
+				String val = (String)e.getValue();
+				key = saveConvert(key, true, true);
+				/* No need to escape embedded and trailing spaces for value, hence
+				 * pass false to flag.
+				 */
+				val = saveConvert(val, false, true);
+				bw.write(key + "=" + val);
+				bw.newLine();
+			}
+		}
+		bw.flush();
+	}
+
+	private String saveConvert(String theString,
+							   boolean escapeSpace,
+							   boolean escapeUnicode) {
+		int len = theString.length();
+		int bufLen = len * 2;
+		if (bufLen < 0) {
+			bufLen = Integer.MAX_VALUE;
+		}
+		StringBuilder outBuffer = new StringBuilder(bufLen);
+		HexFormat hex = HexFormat.of().withUpperCase();
+		for(int x=0; x<len; x++) {
+			char aChar = theString.charAt(x);
+			// Handle common case first, selecting largest block that
+			// avoids the specials below
+			if ((aChar > 61) && (aChar < 127)) {
+				if (aChar == '\\') {
+					outBuffer.append('\\'); outBuffer.append('\\');
+					continue;
+				}
+				outBuffer.append(aChar);
+				continue;
+			}
+			switch(aChar) {
+				case ' ':
+					if (x == 0 || escapeSpace)
+						outBuffer.append('\\');
+					outBuffer.append(' ');
+					break;
+				case '\t':outBuffer.append('\\'); outBuffer.append('t');
+					break;
+				case '\n':outBuffer.append('\\'); outBuffer.append('n');
+					break;
+				case '\r':outBuffer.append('\\'); outBuffer.append('r');
+					break;
+				case '\f':outBuffer.append('\\'); outBuffer.append('f');
+					break;
+				case '=': // Fall through
+				case ':': // Fall through
+				case '#': // Fall through
+				case '!':
+					outBuffer.append('\\'); outBuffer.append(aChar);
+					break;
+				default:
+					if (((aChar < 0x0020) || (aChar > 0x007e)) & escapeUnicode ) {
+						outBuffer.append("\\u");
+						outBuffer.append(hex.toHexDigits(aChar));
+					} else {
+						outBuffer.append(aChar);
+					}
+			}
+		}
+		return outBuffer.toString();
 	}
 }
